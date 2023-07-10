c6cf8c73d8053f124cc4e0df2ec1efbcab319f3b
yurutung <32485239+yurutung@users.noreply.github.com>
2023-07-10 09:34:23 +0800

    change choose course

--- for-debbie.js
+++ for-debbie.js
@@ -1,4 +1,7 @@
-document.getElementById("class_selector").value = "V5<6>JN:;<=>?B=BD@EGCI";
+var course = ["", "V5<6>JN:;<=>?B=BD@EGCI", "V5<6>JO:;<=>?B=BD@EICI", "V5<6>J:;<=>BD=BD@EHCI", "V5<77JNM;<=>?A=BE@EGCI"]
+// 1->經濟, 2->財管, 3->統計, 4->計量經濟
+// 改course後面的數字
+document.getElementById("class_selector").value = course[1];
 getBranch();
 // 記得換日期
 document.getElementById("date_selector").value = "2023-07-13";
