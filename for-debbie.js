dad3b9c7fbfa73bceac0cc516037be19692e3d32
yurutung <32485239+yurutung@users.noreply.github.com>
2023-07-27 18:08:51 +0800

    Update for-debbie.js

--- for-debbie.js
+++ for-debbie.js
@@ -1,5 +1,8 @@
-var course = ["", "V5<6>JN:;<=>?B=BD@EGCI", "V5<6>JO:;<=>?B=BD@EICI", "V5<6>J:;<=>BD=BD@EHCI", "V5<77JNM;<=>?A=BE@EGCI"]
-// 1->經濟, 2->財管, 3->統計, 4->計量經濟
+var course = ["", "V5<6>JO:;<=>?B=BD@EICI", "V5<6>J:;<=>BD=BD@EHCI", "V5<77JN:;<=>?C=BD@EGCI", "V5<77JNM;<=>?A=BE@EGCI"]
+// 1->財管(張翔)-->3594/分鐘   有效日期:2023-12-31 
+// 2->統計學(張翔)-->7908/分鐘   有效日期:2023-12-31
+// 3->經濟學(牧翰)-->11362/分鐘   有效日期:2023-12-31
+// 4->計量經濟學(張翔)-->3438/分鐘   有效日期:2023-12-31
 // 改course後面的數字
 document.getElementById("class_selector").value = course[1];
 getBranch();
